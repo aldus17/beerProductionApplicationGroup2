82273609f999a8c71ee2c9be58bcc4e672076407
Aleksander <aldus17@student.sdu.dk>
2019-10-23 09:22:44 +0200

    Upload folders

---
+++ java/src/main/java/com/mycompany/brewsoft/management/domain/applicationFacade/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.brewsoft.management.domain.applicationFacade;
+
+/**
+ *
+ * @author ALEKSTUD
+ */
+public class Test {
+    
+}
