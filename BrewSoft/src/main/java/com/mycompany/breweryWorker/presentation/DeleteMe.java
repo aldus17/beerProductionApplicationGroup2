2cdc00bbdb93b4dcdaa965985c6c0f701f4c55f0
Aleksander Henriksen <ah@vittech.dk>
2019-10-24 08:58:53 +0200

    Created .java files

---
+++ BrewSoft/src/main/java/com/mycompany/breweryWorker/presentation/DeleteMe.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.breweryWorker.presentation;
+
+import com.mycompany.breweryWorker.domain.*;
+
+/**
+ *
+ * @author a_hen
+ */
+public class DeleteMe {
+    
+}
---
+++ BrewSoft/src/main/java/com/mycompany/data/interfaces/DeleteMe.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.data.interfaces;
+
+import com.mycompany.breweryWorker.domain.*;
+
+/**
+ *
+ * @author a_hen
+ */
+public class DeleteMe {
+    
+}
