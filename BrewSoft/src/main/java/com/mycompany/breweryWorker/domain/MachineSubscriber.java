5586969791e40f84a1cc5bf6190d0541de98c804
Aleksander <aldus17@student.sdu.dk>
2019-10-25 10:14:31 +0200

    Created subscribers for ingredients, products and maintenance.

--- BrewSoft/src/main/java/com/mycompany/breweryWorker/domain/MachineSubscriber.java
+++ BrewSoft/src/main/java/com/mycompany/breweryWorker/domain/MachineSubscriber.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.mycompany.breweryWorker.domain;
 
 import java.util.ArrayList;
@@ -16,6 +15,7 @@ import java.util.logging.Logger;
 import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
 import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
 import org.eclipse.milo.opcua.stack.core.AttributeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.ByteString;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
@@ -32,19 +32,19 @@ import org.eclipse.milo.opcua.stack.core.types.structured.ReadValueId;
  * @author Andreas
  */
 public class MachineSubscriber {
-    
+
     private static final AtomicLong clientHandles = new AtomicLong(1L);
     private MachineConnection mconn;
-    
+
     public MachineSubscriber() {
         this("127.0.0.1", 4840);
     }
-    
+
     public MachineSubscriber(String hostname, int port) {
         mconn = new MachineConnection(hostname, port);
         mconn.connect();
     }
-    
+
     public void subscribe() {
         NodeId batchIdNode = new NodeId(6, "::Program:Cube.Status.Parameter[0].Value");
         NodeId quantityNode = new NodeId(6, "::Program:Cube.Status.Parameter[1].Value");
@@ -56,7 +56,22 @@ public class MachineSubscriber {
         NodeId stopReasonNode = new NodeId(6, "::Program:Cube.Admin.StopReason.Id");
         NodeId stateCurrentNode = new NodeId(6, "::Program:Cube.Status.StateCurrent");
         NodeId machSpeedNode = new NodeId(6, "::Program:Cube.Status.MachSpeed");
-        
+
+        NodeId barleyNode = new NodeId(6, "::Program:Inventory.Barley");
+        NodeId hopsNode = new NodeId(6, "::Program:Inventory.Hops");
+        NodeId maltNode = new NodeId(6, "::Program:Inventory.Malt");
+        NodeId wheatNode = new NodeId(6, "::Program:Inventory.Wheat");
+        NodeId yeastNode = new NodeId(6, "::Program:Inventory.Yeast");
+
+        NodeId maintenanceCounterNode = new NodeId(6, "::Program:Maintenance.Counter");
+        NodeId maintenanceStateNode = new NodeId(6, "::Program:Maintenance.State");
+        NodeId maintenanceTriggerNode = new NodeId(6, "::Program:Maintenance.Trigger");
+
+        NodeId productBadNode = new NodeId(6, "::Program:product.bad");
+        NodeId productGoodNode = new NodeId(6, "::Program:product.good");
+        NodeId productProducedAmountNode = new NodeId(6, "::Program:product.produce_amount");
+        NodeId productProducedNode = new NodeId(6, "::Program:product.produced");
+
         ReadValueId batchIdReadValueId = new ReadValueId(batchIdNode, AttributeId.Value.uid(), null, null);
         ReadValueId quantityReadValueId = new ReadValueId(quantityNode, AttributeId.Value.uid(), null, null);
         ReadValueId tempReadValueId = new ReadValueId(tempNode, AttributeId.Value.uid(), null, null);
@@ -65,8 +80,23 @@ public class MachineSubscriber {
         ReadValueId producedReadValueId = new ReadValueId(producedCountNode, AttributeId.Value.uid(), null, null);
         ReadValueId failedReadValueId = new ReadValueId(failedCountNode, AttributeId.Value.uid(), null, null);
         ReadValueId stopReasonReadValueId = new ReadValueId(stopReasonNode, AttributeId.Value.uid(), null, null);
-        ReadValueId stateReadValueId = new ReadValueId(stateCurrentNode, AttributeId.Value.uid(),null,null);
-        ReadValueId speedReadValueId = new ReadValueId(machSpeedNode, AttributeId.Value.uid(),null,null);
+        ReadValueId stateReadValueId = new ReadValueId(stateCurrentNode, AttributeId.Value.uid(), null, null);
+        ReadValueId speedReadValueId = new ReadValueId(machSpeedNode, AttributeId.Value.uid(), null, null);
+
+        ReadValueId barleyReadValueId = new ReadValueId(barleyNode, AttributeId.Value.uid(), null, null);
+        ReadValueId hopsReadValueId = new ReadValueId(hopsNode, AttributeId.Value.uid(), null, null);
+        ReadValueId maltReadValueId = new ReadValueId(maltNode, AttributeId.Value.uid(), null, null);
+        ReadValueId wheatReadValueId = new ReadValueId(wheatNode, AttributeId.Value.uid(), null, null);
+        ReadValueId yeastReadValueId = new ReadValueId(yeastNode, AttributeId.Value.uid(), null, null);
+
+        ReadValueId maintenanceCounterReadValueId = new ReadValueId(maintenanceCounterNode, AttributeId.Value.uid(), null, null);
+        ReadValueId maintenanceStateReadValueId = new ReadValueId(maintenanceStateNode, AttributeId.Value.uid(), null, null);
+        ReadValueId maintenanceTriggerReadValueId = new ReadValueId(maintenanceTriggerNode, AttributeId.Value.uid(), null, null);
+
+        ReadValueId productBadReadValueId = new ReadValueId(productBadNode, AttributeId.Value.uid(), null, null);
+        ReadValueId productGoodReadValueId = new ReadValueId(productGoodNode, AttributeId.Value.uid(), null, null);
+        ReadValueId productProducedAmountReadValueId = new ReadValueId(productProducedAmountNode, AttributeId.Value.uid(), null, null);
+        ReadValueId productProducedReadValueId = new ReadValueId(productProducedNode, AttributeId.Value.uid(), null, null);
 
         // important: client handle must be unique per item
         UInteger batchIdClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
@@ -80,81 +110,179 @@ public class MachineSubscriber {
         UInteger stateClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
         UInteger speedClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
 
+        UInteger barleyClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger hopsClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger maltClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger wheatClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger yeastClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+
+        UInteger maintenanceCounterClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger maintenanceStateClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger maintenanceTriggerClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+
+        UInteger productBadClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger productGoodClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger productProducedAmountClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+        UInteger productProducedClientHandle = Unsigned.uint(clientHandles.getAndIncrement());
+
         MonitoringParameters batchIdParameters = new MonitoringParameters(
-            batchIdClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                batchIdClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
-        
+
         MonitoringParameters quantityParameters = new MonitoringParameters(
-           quantityClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                quantityClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
         MonitoringParameters tempParameters = new MonitoringParameters(
-           tempClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                tempClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
         MonitoringParameters humidityParameters = new MonitoringParameters(
-           humidityClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                humidityClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
         MonitoringParameters vibrationParameters = new MonitoringParameters(
-           vibrationClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                vibrationClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
         MonitoringParameters producedParameters = new MonitoringParameters(
-            producedClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                producedClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
         MonitoringParameters failedParameters = new MonitoringParameters(
-            failedClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                failedClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
-        
+
         MonitoringParameters stopReasonParameters = new MonitoringParameters(
-            stopReasonClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                stopReasonClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
-        
+
         MonitoringParameters stateParameters = new MonitoringParameters(
-            stateClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                stateClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
-        
+
         MonitoringParameters speedParameters = new MonitoringParameters(
-            speedClientHandle,
-            1000.0,     // sampling interval
-            null,       // filter, null means use default
-            Unsigned.uint(10),   // queue size
-            true        // discard oldest
+                speedClientHandle,
+                1000.0, // sampling interval
+                null, // filter, null means use default
+                Unsigned.uint(10), // queue size
+                true // discard oldest
         );
-        
+        MonitoringParameters barleyParameters = new MonitoringParameters(
+                barleyClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters hopsParameters = new MonitoringParameters(
+                hopsClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters maltParameters = new MonitoringParameters(
+                maltClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters wheatParameters = new MonitoringParameters(
+                wheatClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters yeastParameters = new MonitoringParameters(
+                yeastClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters maintenanceCounterParameters = new MonitoringParameters(
+                maintenanceCounterClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters maintenanceStateParameters = new MonitoringParameters(
+                maintenanceStateClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters maintenanceTriggerParameters = new MonitoringParameters(
+                maintenanceTriggerClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters productBadParameters = new MonitoringParameters(
+                productBadClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters productGoodParameters = new MonitoringParameters(
+                productGoodClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters productProducedAmountParameters = new MonitoringParameters(
+                productProducedAmountClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
+        MonitoringParameters productProducedParameters = new MonitoringParameters(
+                productProducedClientHandle,
+                1000.0,
+                null,
+                Unsigned.uint(10),
+                true
+        );
         List<MonitoredItemCreateRequest> requestList = new ArrayList();
         //requestList.add(new MonitoredItemCreateRequest(batchIdReadValueId, MonitoringMode.Reporting, batchIdParameters));
         //requestList.add(new MonitoredItemCreateRequest(quantityReadValueId, MonitoringMode.Reporting, quantityParameters));
@@ -166,16 +294,16 @@ public class MachineSubscriber {
         //requestList.add(new MonitoredItemCreateRequest(stopReasonReadValueId, MonitoringMode.Reporting, stopReasonParameters));
         requestList.add(new MonitoredItemCreateRequest(stateReadValueId, MonitoringMode.Reporting, stateParameters));
         //requestList.add(new MonitoredItemCreateRequest(speedReadValueId, MonitoringMode.Reporting, speedParameters));
-        
+
         BiConsumer<UaMonitoredItem, Integer> onItemCreated = (item, id) -> item.setValueConsumer(MachineSubscriber::onSubscribeValue);
-        
+
         try {
             UaSubscription subscription = mconn.getClient().getSubscriptionManager().createSubscription(1000.0).get();
             List<UaMonitoredItem> items = subscription.createMonitoredItems(TimestampsToReturn.Both, requestList, onItemCreated).get();
             for (UaMonitoredItem item : items) {
                 if (item.getStatusCode().isGood()) {
                     System.out.println("item created for nodeId=" + item.getReadValueId().getNodeId());
-                } else{
+                } else {
                     System.out.println("failed to create item for nodeId=" + item.getReadValueId().getNodeId() + " (status=" + item.getStatusCode() + ")");
                 }
             }
@@ -185,8 +313,9 @@ public class MachineSubscriber {
             Logger.getLogger(MachineSubscriber.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     private static void onSubscribeValue(UaMonitoredItem item, DataValue value) {
         System.out.println("Item: " + item.getReadValueId().getNodeId() + " Value: " + value.getValue());
     }
+
 }
