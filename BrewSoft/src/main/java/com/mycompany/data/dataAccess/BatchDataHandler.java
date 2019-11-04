aa43ac93e46b99d11fff872a135008912189c687
Jacob <Jacso18@student.sdu.dk>
2019-11-04 17:20:54 +0100

    Insetion to database
    The query has been written.
    - Batch ID still needs proper implementation.
    - The connection to the database still needs to be implemented

--- BrewSoft/src/main/java/com/mycompany/data/dataAccess/BatchDataHandler.java
+++ BrewSoft/src/main/java/com/mycompany/data/dataAccess/BatchDataHandler.java
@@ -5,21 +5,47 @@
  */
 package com.mycompany.data.dataAccess;
 
+import com.mycompany.data.dataAccess.Connect.DBConnections;
 import com.mycompany.data.interfaces.IBatchDataHandler;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author jacob
  */
-public class BatchDataHandler implements IBatchDataHandler {
-
-    @Override
-    public void queueBatch(int typeofProduct, int amountToProduce, double speed, LocalDate deadline) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        //TODO: Use queue batch method
+public class BatchDataHandler extends DBConnections implements IBatchDataHandler {
+    
+    private final String QUEUED_STATUS = "Queued"; 
+    private Random random = new Random();
+    private DBConnections dbConn;
+    
+    //TODO: Implement connections, so that a new connection is not established on each call to the database. 
+    public BatchDataHandler(){
+        dbConn = new DBConnections();
     }
     
-    //TODO: implement queue batch method to database. 
-    
+    @Override
+    public void queueBatch(int typeofProduct, int amountToProduce, double speed, LocalDate deadline) {
+        insertBatchToQueue(typeofProduct, amountToProduce, speed, deadline);
+    }
+    //TODO: Change statement 1 & 6 to apropriate implementations.
+    private void insertBatchToQueue(int typeofProduct, int amountToProduce, double speed, LocalDate deadline){
+        try(PreparedStatement pstBatchToQueue = dbConnection.prepareStatement("INSERT INTO ProductionList VALUES(?,?,?,?,?,?")){
+            pstBatchToQueue.setInt(1,random.nextInt(65335)); //TODO: Implement batchID
+            pstBatchToQueue.setInt(2,typeofProduct);
+            pstBatchToQueue.setFloat(3, amountToProduce);
+            pstBatchToQueue.setDate(4, Date.valueOf(deadline));
+            pstBatchToQueue.setDouble(5, speed);
+            pstBatchToQueue.setString(6, QUEUED_STATUS);
+            pstBatchToQueue.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(BatchDataHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
 }
