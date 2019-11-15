9bbf1de639b22ec508deb75844d949a4d857c027
jonasskottnielsen <43139455+jonasskottnielsen@users.noreply.github.com>
2019-11-15 09:13:08 +0100

    changes

--- BrewSoft/src/main/java/com/mycompany/data/dataAccess/BatchDataHandler.java
+++ BrewSoft/src/main/java/com/mycompany/data/dataAccess/BatchDataHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.data.dataAccess;
 
 import com.mycompany.crossCutting.objects.Batch;
@@ -13,10 +8,6 @@ import com.mycompany.data.interfaces.IBatchDataHandler;
 import java.sql.Date;
 import java.util.ArrayList;
 
-/**
- *
- * @author jacob
- */
 public class BatchDataHandler implements IBatchDataHandler {
 
     private final String QUEUED_STATUS = "Queued";
@@ -42,7 +33,7 @@ public class BatchDataHandler implements IBatchDataHandler {
                 QUEUED_STATUS
         );
     }
-    
+
     //TODO: Implement method that gets the latest queued batch id. 
     // if queue is empty, look at the last produced batch.
     @Override
@@ -52,43 +43,40 @@ public class BatchDataHandler implements IBatchDataHandler {
             return null;
         } else {
             Batch batch = null;
-            for (int i = 0; i < batchSet.getRows(); i++){
+            for (int i = 0; i < batchSet.getRows(); i++) {
                 batch = new Batch(
-                       String.valueOf(batchSet.get(i, "batchid")),
-                       String.valueOf(batchSet.get(i, "productid")),
-                       String.valueOf(batchSet.get(i, "productamount")),
-                       String.valueOf(batchSet.get(i, "deadline")),
-                       String.valueOf(batchSet.get(i, "speed"))   
+                        String.valueOf(batchSet.get(i, "batchid")),
+                        String.valueOf(batchSet.get(i, "productid")),
+                        String.valueOf(batchSet.get(i, "productamount")),
+                        String.valueOf(batchSet.get(i, "deadline")),
+                        String.valueOf(batchSet.get(i, "speed"))
                 );
             }
             return new Integer(batch.getStringBatchID());
         }
     }
-    
- // private helper-method to convert simpleSet to arrayList
-    
-         private ArrayList<BatchReport> simpleSetToArrayList(SimpleSet set){
-            ArrayList<BatchReport> list = new ArrayList<>();
-            set = new SimpleSet();
-            for(int i = 0; i < set.getRows(); i++){
-                BatchReport br = new BatchReport(
-                (int) set.get(i, "finalBatchInformationID"),
-                   (int) set.get(i, "productionListID"),
-                   (int) set.get(i, "BreweryMachineID"),
+
+    // private helper-method to convert simpleSet to arrayList
+    private ArrayList<BatchReport> simpleSetToArrayList(SimpleSet set) {
+        ArrayList<BatchReport> list = new ArrayList<>();
+        set = new SimpleSet();
+        for (int i = 0; i < set.getRows(); i++) {
+            BatchReport br = new BatchReport(
+                    (int) set.get(i, "finalBatchInformationID"),
+                    (int) set.get(i, "productionListID"),
+                    (int) set.get(i, "BreweryMachineID"),
                     set.get(i, "deadline").toString(),
                     set.get(i, "dateOfCreation").toString(),
                     set.get(i, "dateOfCompletion").toString(),
-                   (int) set.get(i, "productID"),
-                   (int) set.get(i, "totalCount"),
-                   (int) set.get(i, "defectCount"),
-                   (int) set.get(i, "acceptedCount"));
-                
-                list.add(br);
-            }
-            
-            return list;
+                    (int) set.get(i, "productID"),
+                    (int) set.get(i, "totalCount"),
+                    (int) set.get(i, "defectCount"),
+                    (int) set.get(i, "acceptedCount"));
+
+            list.add(br);
         }
 
-    
+        return list;
+    }
 
 }
--- BrewSoft/src/main/java/com/mycompany/data/interfaces/IBatchDataHandler.java
+++ BrewSoft/src/main/java/com/mycompany/data/interfaces/IBatchDataHandler.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.data.interfaces;
 
 import com.mycompany.crossCutting.objects.Batch;
-import com.mycompany.data.dataAccess.Connect.SimpleSet;
-import java.time.LocalDate;
 
-/**
- *
- * @author jacob
- */
 public interface IBatchDataHandler {
  
      //public void insertBatchToQueue(int typeofProduct, int amountToProduce, double speed, LocalDate deadline);
