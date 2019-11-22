e83bb49927617969d4699a20cd9e6867d4108f20
JacobGlumby1 <43093373+JacobGlumby1@users.noreply.github.com>
2019-11-22 10:04:52 +0100

    Removed redundant method

--- BrewSoft/src/main/java/com/mycompany/data/interfaces/IBatchDataHandler.java
+++ BrewSoft/src/main/java/com/mycompany/data/interfaces/IBatchDataHandler.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 public interface IBatchDataHandler {
 
-     //public void insertBatchToQueue(int typeofProduct, int amountToProduce, double speed, LocalDate deadline);
      public void insertBatchToQueue(Batch batch);
 
      public ArrayList<Batch> getQueuedBatches();
